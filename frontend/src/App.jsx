210c6afd5549de601608207708fbd5358fda5251
voja <voja@gmail.com>
2024-06-01 11:00:51 +0200

    07 Header Footer and App Markup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,23 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
-    return <div>App</div>;
+    return (
+        <>
+            <Header />
+            <div className="container mt-3">
+                <div className="row">
+                    <div className="col-4">
+                        <h4>Categories</h4>
+                    </div>
+                    <div className="col">
+                        <h4>Products</h4>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <footer>
+            <p>
+                &copy; {new Date().getFullYear()} Shirts & Fruit. All rights
+                reserved.
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
